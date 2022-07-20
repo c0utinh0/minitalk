cdada5f0dcfe309761362aba13d873db61b6ee9e
c0utinh0 <douglas-couto@hotmail.com>
2022-07-20 14:11:48 -0300

    InProgress2007

--- client.c
+++ client.c
@@ -6,15 +6,25 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:07:41 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/07/19 19:07:55 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/07/20 14:09:01 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
 #include <signal.h>
+#include <stdlib.h> //Remover
+#include <stdio.h> //Remover
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	kill(21113, SIGINT);
+	int	pid;
+	char	*str;
+	
+	str = argv[2];
+	printf("%s\n", str);
+	pid = atoi(argv[1]); // Mudar para ft_atoi
+	printf("%d\n", pid);
+	printf("%c\n", str[0]);
+	printf("%d\n", str[0]);
+	kill(pid, SIGINT);
 	return(0);
 }
--- server.c
+++ server.c
@@ -6,14 +6,16 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:08:11 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/07/19 19:08:16 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/07/20 11:43:52 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h> //Remover
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	printf("%d\n", getpid()); //Mudar para ft_printf
 	while(1)
 		pause();
 	return(0);
