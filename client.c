e5ba09b8a177283e7c20e4eb64a6b929f0515e7e
c0utinh0 <douglas-couto@hotmail.com>
2022-07-21 16:00:13 -0300

    Server recebendo os bits

--- client.c
+++ client.c
@@ -6,25 +6,47 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:07:41 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/07/20 14:09:01 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:48:20 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdlib.h> //Remover
 #include <stdio.h> //Remover
+#include <unistd.h> //Remover
+
+void	ft_char_to_bin(char c, pid_t pid)
+{
+	int	i;
+
+	i = 7;	
+	while(i >= 0)
+	{
+		if(c & (1 << i))
+		{
+			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
+		usleep(10);
+		i--;
+	}
+}
 
 int	main(int argc, char *argv[])
 {
-	int	pid;
+	pid_t	pid;
 	char	*str;
 	
 	str = argv[2];
-	printf("%s\n", str);
 	pid = atoi(argv[1]); // Mudar para ft_atoi
-	printf("%d\n", pid);
-	printf("%c\n", str[0]);
-	printf("%d\n", str[0]);
-	kill(pid, SIGINT);
+	
+	while(*str)
+	{
+		ft_char_to_bin(*str, pid);
+		str++;
+	}
 	return(0);
 }
--- server.c
+++ server.c
@@ -6,16 +6,30 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:08:11 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/07/20 11:43:52 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:57:46 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h> //Remover
+#include <signal.h>
+#include <stdlib.h>
+
+static void sig_usr(int signo) /* o argumento indica o sinal recebido */
+{
+	if (signo == SIGUSR1)
+		write(1, "1", 1);
+	else if (signo == SIGUSR2)
+		write(1, "0", 1);
+	return;
+}
 
 int	main(int argc, char *argv[])
 {
 	printf("%d\n", getpid()); //Mudar para ft_printf
+	signal(SIGUSR1, sig_usr); 
+	signal(SIGUSR2, sig_usr); 
+	
 	while(1)
 		pause();
 	return(0);
