d32fd21443b0bceeccfadba7a8259ea6c9f57638
dcoutinh <douglas-couto@hotmail.com>
2022-08-01 10:52:30 -0300

    Finalizado com bonus

--- client_bonus.c
+++ client_bonus.c
@@ -6,12 +6,14 @@
 /*   By: dcoutinh <dcoutinh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:07:41 by dcoutinh          #+#    #+#             */
-/*   Updated: 2022/07/29 18:16:47 by dcoutinh         ###   ########.fr       */
+/*   Updated: 2022/08/01 10:43:38 by dcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft/libft.h"
+
+int	g_count;
 
 static int	send_str(int pid, char *s)
 {
@@ -28,6 +30,7 @@ static int	send_str(int pid, char *s)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
+			pause();
 			usleep(200);
 			j--;
 		}
@@ -37,25 +40,48 @@ static int	send_str(int pid, char *s)
 	return (0);
 }
 
-int	main(int argc, char *argv[])
+static void	client_handler(int signal, siginfo_t *siginfo)
 {
-	pid_t	pid;
-	char	*str;
+	if (signal == SIGUSR1)
+		g_count++;
+	return ;
+}
 
-	(void) argc;
-	str = argv[2];
-	pid = atoi(argv[1]);
+int	ft_check_errors(int argc, char *pid_s, char *str)
+{
+	pid_t				pid;
+
+	pid = atoi(pid_s);
 	if (pid < 0)
 	{
 		ft_printf("Wrong PID!\n");
-		return (0);
+		return (1);
 	}
 	if (argc == 3)
 		send_str(pid, str);
 	else
 	{
 		ft_printf("Wrong arguments\n");
-		return (0);
+		return (1);
 	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int					ret;
+	sigset_t			set;
+	struct sigaction	action;
+
+	g_count = 0;
+	sigemptyset(&set);
+	action.sa_sigaction = (void *)client_handler;
+	action.sa_flags = SA_SIGINFO;
+	action.sa_mask = set;
+	sigaction(SIGUSR1, &action, NULL);
+	(void) argc;
+	if (ft_check_errors(argc, argv[1], argv[2]) == 1)
+		return (1);
+	ft_printf("The server received %d characters\n", (g_count / 8));
 	return (0);
 }
